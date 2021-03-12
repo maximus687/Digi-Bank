f6b7ce8efdfde88c6b108f76a31f75832735b7a5
Ken Sodemann <ken@ionic.io>
2021-03-12 07:05:36 -0600

    chore(): use Capacitor version 3

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,6 +7,9 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string;
+  password: string;
+
   constructor(private navController: NavController) {}
 
   ngOnInit() {}
